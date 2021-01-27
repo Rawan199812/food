e67bd84be28a392692c52c8562e657003a0f60c3
Rawan199812 <rawan.13579@icloud.com>
2021-01-27 17:48:50 +0200

    i began to learn JS

---
+++ app.js
@@ -0,0 +1,16 @@
+console .log ('welcome to Food');
+var message='';
+
+alert('welcome to Food');
+var age=prompt('How old are you?');
+if (age<=18){ 
+    message='You needs1800 Calories To Eat Every Day ';
+}else if (age>=19){
+    message='You Need 2000 Calories To Eat Every Day';
+
+}
+document.write('<h2>'+ message +'</h2');
+confirm('Do You Like Our services? ')
+alert('Have A Nice Day');
+
+
