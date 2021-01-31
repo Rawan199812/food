bd43887a9a065ff87633ef3ae68c5b1129b6bf1d
Rawan199812 <rawan.13579@icloud.com>
2021-01-31 17:10:45 +0200

    practice css and function JS

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 console .log ('welcome to Food');
-var message='';
+function welcomeMessage() {
+    var message='';
 
-alert('welcome to Food');
-var age=prompt('How old are you?');
-if (age<=18){ 
-    message='You needs1800 Calories To Eat Every Day ';
-}else if (age>=19){
-    message='You Need 2000 Calories To Eat Every Day';
-
+    alert('welcome to Food');
+    var age=prompt('How old are you?');
+    if (age<=18){ 
+        message='You needs1800 Calories To Eat Every Day ';
+    }else if (age>=19){
+        message='You Need 2000 Calories To Eat Every Day';
+       
 }
-document.write('<h2>'+ message +'</h2>');
 
+return document.write('<h2>'+ message +'</h2>');
+}
 confirm('Do You Like Our services? ')
 alert('Have A Nice Day');
 
